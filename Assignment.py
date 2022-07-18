2b22a2c29ee2082b199ccd6246108246bdf5a06f
Mokshitha289 <109502088+Mokshitha289@users.noreply.github.com>
2022-07-18 15:43:53 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hi I'm Mokshitha")
+
+
+# In[6]:
+
+
+8*9+(2-8)
+
+
+# In[7]:
+
+
+num = 9
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+
+# In[ ]:
+
+
+
+
